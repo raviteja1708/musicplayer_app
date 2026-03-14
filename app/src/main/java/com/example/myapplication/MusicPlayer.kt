bbb0e0b6275821c660f4ebc4b37708ba1ee20c47
RaviTeja <chintharavi9995@gmail.com>
2026-03-14 15:25:21 +0530

    Updated

--- app/src/main/java/com/example/myapplication/MusicPlayer.kt
+++ app/src/main/java/com/example/myapplication/MusicPlayer.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -55,6 +56,7 @@ class MusicPlayer : Fragment() {
         }
         binding.songlist.setOnClickListener {
             val songListFragment = MusicList()
+            Log.d("Ravi","This is Ravit")
             parentFragmentManager.beginTransaction()
                 .replace(R.id.main_container, songListFragment)
                 .addToBackStack(null)
